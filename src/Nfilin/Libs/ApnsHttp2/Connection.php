43df607dc697c305aaff697e8efbb3f0bb1fa85d
Stanislav Markovskyi <smarkovskyi@zeuselectronics.eu>
2016-03-16 16:53:13 +0200

    APNS headers

--- src/Nfilin/Libs/ApnsHttp2/Connection.php
+++ src/Nfilin/Libs/ApnsHttp2/Connection.php
@@ -80,6 +80,19 @@ class Connection extends CurlConnection
             $headers = [];
             $headers['content-length'] = strlen($data);
             $headers['apns-topic'] = $message->topic;
+            $headers['apns-expiration'] = $message->time_to_live ? (int)$message->time_to_live + time() : 0;
+            switch ($message->priority) {
+                case 5:
+                case 'normal':
+                    $headers['apns-priority'] = 5;
+                    break;
+                case 10:
+                case 'high':
+                    $headers['apns-priority'] = 10;
+                    break;
+                default:
+                    $headers['apns-priority'] = 10;
+            }
 
             curl_setopt($this->curl, CURLOPT_POSTFIELDS, $data);
             curl_setopt($this->curl, CURLOPT_HTTPHEADER, $headers);
